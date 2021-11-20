ab0b0b8043b0c39b07ead0b3657c31e68e2888d4
Angst <57171010+Skuzee@users.noreply.github.com>
2021-11-20 01:09:06 -0500

    Added diagonal notch compensation. For now it is only diagonals, and the values are manually entered into an array. Working on automatic notch identification and calibration and will need to add cardinal notch compensation. Will probably move this code to ESS.c or make a new file.

--- ESS-Adapter/ESS-Adapter.ino
+++ ESS-Adapter/ESS-Adapter.ino
@@ -34,20 +34,8 @@
 #if NINTENDO_VERSION != 1337
 #error "Incorrect Nintendo.h library! Compiling with the incorrect version WILL result in 5 volts being output to your controller/console! (Not good.) Make sure the custom Nintendo library (version 1337) is included in the ESS-Adapter/src folder and try again."
 #endif
-
-#define Q1_NOTCH_X 77
-#define Q1_NOTCH_Y 82
-
-#define Q2_NOTCH_X -70
-#define Q2_NOTCH_Y 79
-
-#define Q3_NOTCH_X -72
-#define Q3_NOTCH_Y -76
-
-#define Q4_NOTCH_X 78
-#define Q4_NOTCH_Y -78
-													//Q1	  //Q2			//Q3			//Q4
-int8_t cornerNotches[] = {77, 82, -70, 79, -72, -76, 78, -78};
+													//Q1	  //Q2			//Q3		 //Q4
+int8_t cornerNotches[] = {77, 80, -70, 79, -72, -76, 78, -78};
 
 // Sets CONT_PIN on arduino to read from controller.
 CGamecubeController GCcontroller(CONT_PIN);
@@ -208,37 +196,45 @@ uint8_t N64loop() { // Wii vc version of OOT updates controller twice every ~16.
   return N64controller.getDevice();
 }
 
+// Applies a slight correction to the diagonal corner values to nudge the analog value towards the ideal 45 degree angle. The end result is a correction proportional to the closeness of the current coordinate to the physical notch.
 void notchCorrection(uint8_t ucoords[2]) {
-//	uint8_t dist = pythagDist(coords[0],coords[1],NW_NOTCH_X-128,NW_NOTCH_Y-128);
-
-	// if(dist <= abs(notchCorrectionNW)) {
-	// 	coords[0] += (dist + notchCorrectionNW);
-	// 	coords[1] += (dist + notchCorrectionNW);
-	// }
 
 
-	// int8_t diff = (abs(ucoords[0]-128) - abs(ucoords[1]-128));
-	// if (ucoords[0] == 128 || ucoords[1] == 128)
-	// 	diff = 0;
-	// ucoords[0] -= diff*constrain(ucoords[0]-128,-1,1)/2;
-	// ucoords[1] += diff*constrain(ucoords[1]-128,-1,1)/2;
-
-	//remember un/signed
 	//calc dist
 	//constrain dist to 0 to diff/2
 	//reverse map: diff to 0/2
 	//add that as correction offset to X and Y
-	for (uint8_t i = 0; i<8; i+=2) {
 
-		int8_t diff = abs(cornerNotches[i]) - abs(cornerNotches[i+1]);
-		uint8_t sign = constrain(diff,-1,1);
-		diff = abs(diff);
-		uint8_t dist = pythagDist(ucoords[0],ucoords[1],cornerNotches[i]+128,cornerNotches[i+1]+128);
-		dist = constrain(dist,0,diff/2);
-		dist = map(dist, 0, diff/2, diff/2, 0);
+	for (uint8_t i = 0; i<8; i+=2) { // Calculate for each of 4 corner notches.
 
-		ucoords[0] -= dist*constrain(ucoords[0]-128,-1,1)*sign;
-		ucoords[1] += dist*constrain(ucoords[1]-128,-1,1)*sign;
+		// Calculate the correction factor to apply to make X == Y
+		// if corner is 70,78 (unsigned values) the ideal diagonal is 74,74
+		// which is the average.
+		// Calculate X and Y seperately. In case the difference is odd, round 1 up and 1 down.
+		int8_t diffX = ceil(float(abs(cornerNotches[i]) - abs(cornerNotches[i+1]))/2);
+		int8_t diffY = floor(float(abs(cornerNotches[i]) - abs(cornerNotches[i+1]))/2);
+		// Save whether diff is pos/neg. We need it later.
+		uint8_t sign = constrain(diffX,-1,1);
+		// Remove sign to apply scaling map.
+		diffX = abs(diffX);
+		diffY = abs(diffY);
+		// Calculate the distance between current coordinates and corner notch.
+		uint8_t distX = pythagDist(ucoords[0],ucoords[1],cornerNotches[i]+128,cornerNotches[i+1]+128);
+		uint8_t distY = distX;
+		// Constrain the max value of dist to the difference.
+		distX = constrain(distX,0,diffX);
+		distY = constrain(distY,0,diffY);
+		// Invert the map. This means that distance starts at "0" and the closer we get
+		// to the corner notch the higher the value gets. The correction factor is strongest
+		// when we are at the corner notch, and fades as we get farther.
+		distX = map(distX, 0, diffX, diffX, 0);
+		distY = map(distY, 0, diffY, diffY, 0);
+		// The correction factor is to nudge the coordinate towards the true 45 line.
+		// We need to add/subtract depending whether the correction factor is negative
+		// or positive. (A negative dist means X < Y. We also need to flip the correction
+		// if the axis is negative.
+		ucoords[0] -= distX*constrain(ucoords[0]-128,-1,1)*sign;
+		ucoords[1] += distY*constrain(ucoords[1]-128,-1,1)*sign;
 	}
 }
 
